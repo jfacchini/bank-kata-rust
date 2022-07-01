8312db7ee6e4c4239022e2395a6a2e889fed5b47
Jocelyn Facchini <jocelyn.facchini@codurance.com>
2022-07-01 23:12:22 +0100

    Starting point

---
+++ tests/bank_acceptance_test.rs
@@ -0,0 +1,29 @@
+struct Console {
+    buffer: String
+}
+impl Console {
+    fn new() -> Self {
+        Self {
+            buffer: String::new(),
+        }
+    }
+}
+
+#[test]
+fn print_statement_lists_the_account_transactions_in_reverse_chronological_order() {
+    let console = Console::new();
+    let account = Account::new();
+
+    account.deposit(1000);
+    account.deposit(2000);
+    account.withdraw(500);
+    account.print_statement();
+
+    assert_eq!(console.buffer, format!(
+        "{}\n{}\n{}\n{}\n",
+        "Date || Amount || Balance",
+        "14/01/2012 || -500 || 2500",
+        "13/01/2012 || 2000 || 3000",
+        "10/01/2012 || 1000 || 1000"
+    ))
+}
